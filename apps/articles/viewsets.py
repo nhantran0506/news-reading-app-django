308132593fb55eeaf41d995a3afb164bff0faadc
NhanPham03 <ph.nhan03@gmail.com>
2024-05-20 09:13:51 +0700

    Add Article retrieve filter

--- apps/articles/viewsets.py
+++ apps/articles/viewsets.py
@@ -7,3 +7,9 @@ class ArticleViewSet(viewsets.ModelViewSet):
   queryset = Article.objects.all()
   serializer_class = ArticleSerializer
   permission_classes = [AllowAny]
+
+  def get_queryset(self):
+    user_id = self.request.query_params.get('userID')
+    if user_id:
+      return Article.objects.filter(user__id=user_id)
+    return super().get_queryset()
