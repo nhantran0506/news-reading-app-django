2891a8f6f9970a5da476ab8381e9c16b41ec2643
Trần Trọng Nhân <trant.nhan2003@gmail.com>
2024-05-25 21:28:27 +0700

    fix comment

--- apps/comments/serializers.py
+++ apps/comments/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from apps.comments.models import Comment
-from apps.users.serializers import UserSerializer  # Import the UserSerializer
+from apps.users.models import User
 
 class CommentSerializer(serializers.ModelSerializer):
-    # Include the user serializer to retrieve the username
-    user = UserSerializer(read_only=True)
-    username = serializers.ReadOnlyField(source='user.username')  # Add the username field
+    user_id = serializers.ReadOnlyField(source='user.id')  # Include user_id
+    username = serializers.ReadOnlyField(source='user.username')  # Include username
 
     class Meta:
         model = Comment
-        fields = '__all__'
-        read_only_fields = ['created_at', 'updated_at', 'username']  # Make username read-only
+        fields = ['id', 'user_id', 'username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at', 'user_id', 'username', 'created_at', 'updated_at']  # Make user_id and username read-only
         extra_kwargs = {
             'user': {'required': True},
             'article': {'required': True}
