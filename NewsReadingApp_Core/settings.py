a90cf44d7abe876ddd44dc465af3436cf52d5dc7
Trần Trọng Nhân <trant.nhan2003@gmail.com>
2024-05-28 12:52:20 +0700

    page update

--- NewsReadingApp_Core/settings.py
+++ NewsReadingApp_Core/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # Third-party apps
     'rest_framework',
     'corsheaders',
@@ -151,6 +152,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
